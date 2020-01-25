d164f56d50d81fa6d7f6d62f3d79a4f2422c78f0
JAESEUNG LEE <41176085+tkxkd0159@users.noreply.github.com>
2020-01-26 03:12:52 +0900

    add Scroll Back To Top button

---
+++ assets/js/add.js
@@ -0,0 +1,13 @@
+//Get the button
+var mybutton = document.getElementById("myUpBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}

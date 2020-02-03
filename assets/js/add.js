e0354627625b991176d7b9dd05723ba2c9e0db85
JAESEUNG LEE <41176085+tkxkd0159@users.noreply.github.com>
2020-02-04 02:30:22 +0900

    delete some icon

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,7 +1,7 @@
 //Get the button
 var mybutton = document.getElementById("myUpBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 800px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
